aa6530e901b415c89ff0fa3e16e4d9475eeab2b7
yaokaixuan <102214125@ntunhs.edu.tw>
2023-09-14 23:05:57 +0800

    delete

--- app/server.js
+++ app/server.js
@@ -1,11 +1,54 @@
-import controllers from '../controllers/index.js';
 import express from 'express';
+import sql from 'mssql';
+import controllers from '../controllers/index.js';
 import viteExpress from 'vite-express';
+import session from 'express-session';
 
 const app = express();
+app.use(express.json());
 
-app.use('/api', controllers);
+app.use(session({
+    secret: 'yourSecretKey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false, // 改成 true 如果你使用 HTTPS
+        httpOnly: true,
+        maxAge: 6000000 // 1 minute
+    }
+}));
 
-const server = app.listen(3000, () => {});
+const config = {
+    // user: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PASSWORD,
+    // server: process.env.DATABASE_SERVER,
+    // database: process.env.DATABASE_NAME,
+    user: 'sa',
+    password: '<YourStrong@Passw0rd>',
+    server: 'localhost',
+    database: 'BANK',
+    options: {
+        encrypt: true, // 使用 SSL
+        trustServerCertificate: true // 信任自簽名憑證
+    }
+};
 
-viteExpress.bind(app, server);
+// 連接資料庫
+sql.connect(config).then(pool => {
+    console.log('Connected to the database.');
+
+    // 將資料庫 pool 傳遞給你的控制器，如果需要的話
+    app.locals.pool = pool;
+
+    // 使用你的控制器
+    app.use('/api', controllers);
+
+    const server = app.listen(3251, () => {
+        console.log('Server is running on port 3251');
+    });
+
+    viteExpress.bind(app, server);
+
+}).catch(err => {
+    console.error('Database connection failed!', err);
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,53 @@
+/* eslint-disable no-unused-vars */
 import { Router } from "express";
-
+import sql from 'mssql';
 const router = Router();
 
+
+
+router.get('/trans', async (req, res) => {
+    try {
+        // 假設連接池在 app.locals.pool 中可用
+        const pool = req.app.locals.pool;
+
+        // 執行查詢
+        const result = await pool.request().query('SELECT AccID, TranID, CONVERT(varchar, TranTime, 23) AS TranTime, AtmID, TranType, TranNote, CONVERT(varchar, UP_DATETIME, 23) AS UP_DATETIME, UP_USR\n' +
+            'FROM Trans;');
+
+        // 在控制台中打印結果
+        console.log(result);
+
+        // 也可以將結果發送到客戶端
+        res.json(result.recordset);
+    } catch (err) {
+        console.error('Error querying Trans table', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.delete('/trans/:TranID', async (req, res) => {
+    try {
+        const pool = req.app.locals.pool;
+        const { TranID } = req.params;
+
+        const result = await pool.request()
+            .input('TranID', sql.NVarChar, TranID)
+            .query('DELETE FROM Trans WHERE TranID = @TranID');
+
+        if (result.rowsAffected[0] === 0) {
+            res.status(404).send('Transaction not found');
+            return;
+        }
+
+        res.status(200).json({ message: 'Transaction deleted successfully' });
+    } catch (err) {
+        console.error('Error deleting transaction', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 export default router;
+
+
+
+
